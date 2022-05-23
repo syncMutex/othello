ba470c71e36804a8fed7b0ec37d281030aa01973
ProClasic <prateekp.0055@gmail.com>
2022-05-23 21:56:36 +0530

    available moves done

--- client-browser/src/views/game.tsx
+++ client-browser/src/views/game.tsx
@@ -6,6 +6,10 @@ type Board = Array<Array<number>>
 
 const BLACK = 98;
 const WHITE = 119;
+const EMPTY = 0;
+const isDebug = false;
+
+const LEFT = -1, RIGHT = 1, UP = -1, DOWN = 1;
 
 export default function Game() {
   const socket = useContext(SocketContext);
@@ -13,14 +17,18 @@ export default function Game() {
   const [isCurTurn, setIsCurTurn] = useState<boolean>(false);
   const mySide = sessionStorage.side?.charCodeAt(0);
   const opponentSide = (mySide === BLACK) ? WHITE : BLACK;
-  const [availableMovesIdx, setAvailableMovesIdx] = useState<Set<string>>(new Set());
+  const [availableMovesIdxs, setAvailableMovesIdxs] = useState<Set<string>>(new Set());
 
   useEffect(() => {
     document.documentElement.style.setProperty(
-      "--ghost-color", (mySide === BLACK) ? "rgba(0, 0, 0, 0.4)" : "rgba(255, 255, 255, 0.4)"
+      "--ghost-color", (mySide === BLACK) ? "rgb(0, 0, 0)" : "rgb(255, 255, 255)"
     );
     socket.emit("game-state");
     socket.on("game-state-res", (game:{board:Board, curTurn:number}) => {
+      game.board[2][4] = WHITE;
+      game.board[4][0] = WHITE;
+      game.board[4][1] = WHITE;
+      game.board[4][2] = WHITE;
       setBoard(game.board);
       if(mySide === game.curTurn) setIsCurTurn(true);
     })
@@ -36,11 +44,40 @@ export default function Game() {
           (i - 1 < 0)
         ) continue;
 
-        let m = i - 1;
-        while(m >= 0 && (board[m][j] === opponentSide)) m--;
-        console.log(i, m, board[i][m]);
+        const isInBounds = (r:number, c:number):boolean => r >= 0 && r < board.length && c >= 0 && c < board[0].length;
+
+        let row:number, col:number;
+        const traverseVertic = (mag:-1|1) => {
+          row = i + mag;
+          while(isInBounds(row, j) && (board[row][j] === opponentSide)) row += mag;
+          if(board[row][j] === EMPTY && row !== i + mag) newMoves.add(`${row}-${j}`);
+        }
+        traverseVertic(UP);
+        traverseVertic(DOWN);
+        const tranverseHoriz = (mag:1|-1) => {
+          col = j + mag;
+          while(isInBounds(i, col) && (board[i][col] === opponentSide)) col += mag;
+          if(board[i][col] === EMPTY && col !== j + mag) newMoves.add(`${i}-${col}`);
+        }
+        tranverseHoriz(LEFT);
+        tranverseHoriz(RIGHT);
+        // check up-right
+        const traverseDiag = (rMag:1|-1, cMag:1|-1) => {
+          row = i + rMag;
+          col = j + cMag;
+          while(isInBounds(row, col) && (board[row][col] === opponentSide)) {
+            row += rMag;
+            col += cMag;
+          }
+          if(board[row][col] === EMPTY && col !== j + cMag && row !== i + rMag) newMoves.add(`${row}-${col}`);
+        }
+        traverseDiag(UP, RIGHT);
+        traverseDiag(UP, LEFT);
+        traverseDiag(DOWN, LEFT);
+        traverseDiag(DOWN, RIGHT);
       }
     }
+    setAvailableMovesIdxs(newMoves);
   }
 
   useEffect(() => { isCurTurn && checkMoves() }, [isCurTurn])
@@ -57,7 +94,15 @@ export default function Game() {
       {board?.map((row:Array<number>, rIdx:number) =>
         <div key={rIdx} className="row">
           {row.map((rune:number, cellIdx:number) => (
-            <div onClick={() => playMove(rIdx, cellIdx)} key={cellIdx} className={`color-${rune}`}></div>
+            <div
+              onClick={() => playMove(rIdx, cellIdx)}
+              key={cellIdx}
+              className={`color-${rune}${availableMovesIdxs.has(`${rIdx}-${cellIdx}`) ? " available" : ""}`}
+            >
+              {isDebug && <span style={{ position:'absolute', color: "aqua", top: 0, left: 0, zIndex: 10, pointerEvents: "none" }}>
+                {`${rIdx}-${cellIdx}`}
+              </span>}
+            </div>
           ))}
         </div>
       )}
